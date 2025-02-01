acf79e91d607f2a82f44ff08e8bf65f3c01e2800
klude-au <124860775+klude-au@users.noreply.github.com>
2025-02-01 18:44:16 +1030

    update: fixed something very crucial (stupid stuff)

--- fw-update.bat
+++ fw-update.bat
@@ -1,6 +1,2 @@
 @echo off
-
-SET FW__MY_BRANCH=variant-1
-::git -C "%~dp0lib-ace" fetch --all --tags
-::git -C "%~dp0lib-ace" checkout tags/%FW__MY_BRANCH%
-git checkout -b %FW__MY_BRANCH%
+git fetch origin
